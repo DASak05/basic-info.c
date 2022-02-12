ada937908cd8d1a906233d6c3ce049ec040c5159
DASak05 <99540547+DASak05@users.noreply.github.com>
2022-02-12 18:20:47 +0530

    Add files via upload

---
+++ basicinfo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char name[22];
+	char branch[70]; char hobbies[50];
+	int regd;
+	printf("please enter your name\n");
+	scanf("%s",name);
+	printf("please enter your branch\n");
+	scanf("%s",branch);
+	printf("please enter your regd no\n");
+	scanf("%d",&regd);
+	printf("please enter your hobbies\n");
+	scanf("%s",hobbies);
+	return 0;
+}
